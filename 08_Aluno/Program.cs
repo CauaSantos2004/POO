044328376855ab4d8e829c28d32b621c69c923f7
caua.asantos2 <caua.santos.devs@gmail.com>
2025-09-18 20:19:33 -0300

    Tarefa - DeclaraçãoClasse

---
+++ 08_Aluno/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _08_Aluno
+{
+    class Program
+    {
+        // Ponto de entrada do programa
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Cadastro de Aluno ===");
+
+            // Solicita o nome do aluno
+            Console.Write("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            // Solicita a idade do aluno
+            Console.Write("Digite a idade do aluno: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            // Solicita a primeira nota
+            Console.Write("Digite a primeira nota: ");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            // Solicita a segunda nota
+            Console.Write("Digite a segunda nota: ");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            // Cria o objeto aluno
+            Aluno aluno = new Aluno(nome, idade, nota1, nota2);
+
+            // Mostra os dados do aluno
+            Console.WriteLine("\n--- Dados do Aluno ---");
+            Console.WriteLine("Nome: " + aluno.Nome);
+            Console.WriteLine("Idade: " + aluno.Idade);
+            Console.WriteLine("Nota 1: " + aluno.Nota1);
+            Console.WriteLine("Nota 2: " + aluno.Nota2);
+            Console.WriteLine("Média: " + aluno.Media);
+            Console.WriteLine("Situação: " + aluno.Situacao);
+
+            Console.ReadLine(); // pausa antes de fechar
+        }
+    }
+}
