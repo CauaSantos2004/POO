54be19087f35e5cffe9077679ff5a44b6155c6ce
caua.asantos2 <caua.santos.devs@gmail.com>
2025-10-06 22:15:10 -0300

    Aula dia 06/10 segunda feira

---
+++ 20_ValidaCPFComExcecao/CPFMesmoNumeroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_ValidaCPFComExcecao
+{
+    public class CPFMesmoNumeroException : Exception
+    {
+        public CPFMesmoNumeroException() :base("Todos os números do CPF não podem ser iguais!"){}
+    
+        
+    
+    }
+}
